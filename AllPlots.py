5cb3fdc271bf2d99a50649bd7a102bbc7223e9ec
divyakhatiyan <divyakhatiyan@gmail.com>
2025-03-16 23:00:36 +0530

    Plots added

---
+++ AllPlots.py
@@ -0,0 +1,12 @@
+from matplotlib import pyplot as plt
+
+## Alternative import statement
+## import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+# Standard Line Plot
+
+plt.plot(x,y)
+
+#Scatter plot
