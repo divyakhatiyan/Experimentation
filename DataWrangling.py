ddc8626e8afe7926669fe1fdcfec13e9c753e86e
divyakhatiyan <divyakhatiyan@gmail.com>
2025-03-16 05:45:42 +0530

    Data Preprocessing code addded

--- DataWrangling.py
+++ DataWrangling.py
@@ -1,5 +1,7 @@
+
+
 import pandas as pd
-import matplotlib.pylab as plt
+#import matplotlib.pylab as plt
 import numpy as np
 
 
@@ -84,8 +86,117 @@ df['city-L/100km'] = 235/df["city-mpg"]
 # check your transformed data
 df.head()
 
+
 #Conversion to correct data types
 df[["bore", "stroke"]] = df[["bore", "stroke"]].astype("float")
 df[["normalized-losses"]] = df[["normalized-losses"]].astype("int")
 df[["price"]] = df[["price"]].astype("float")
 df[["peak-rpm"]] = df[["peak-rpm"]].astype("float")
+
+
+# Write your code below and press Shift+Enter to execute
+#df['highway-L/100km'] = 235/df['highway-mpg']
+
+# transform mpg to L/100km by mathematical operation (235 divided by mpg)
+df["highway-mpg"] = 235/df["highway-mpg"]
+
+# rename column name from "highway-mpg" to "highway-L/100km"
+df.rename(columns={'"highway-mpg"':'highway-L/100km'}, inplace=True)
+
+# check your transformed data
+df.head()
+
+##Target: normalize those variables so their value ranges from 0 to 1
+
+##Approach: replace the original value by (original value)/(maximum value)
+
+# replace (original value) by (original value)/(maximum value)
+df['length'] = df['length']/df['length'].max()
+df['width'] = df['width']/df['width'].max()
+# Write your code below and press Shift+Enter to execute
+df['height'] = df['height']/df['height'].max()
+
+# show the scaled columns
+df[["length","width","height"]].head()
+
+##Binning
+
+##Conversion to correct format
+
+df["horsepower"]=df["horsepower"].astype(int, copy=True)
+#Plot the histogram of horsepower to see the distribution of horsepower.
+import matplotlib as plt
+from matplotlib import pyplot
+
+plt.pyplot.hist(df["horsepower"])
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+plt.pyplot.show()
+bins = np.linspace(min(df["horsepower"]), max(df["horsepower"]), 4)
+bins
+
+group_names = ['Low', 'Medium', 'High']
+
+df['horsepower-binned'] = pd.cut(df['horsepower'], bins, labels=group_names, include_lowest=True )
+df[['horsepower','horsepower-binned']].head(20)
+
+df["horsepower-binned"].value_counts()
+
+
+#Plot the distribution of each bin:
+pyplot.bar(group_names, df["horsepower-binned"].value_counts())
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+plt.pyplot.show()
+
+
+# draw historgram of attribute "horsepower" with bins = 3
+plt.pyplot.hist(df["horsepower"], bins = 3)
+
+# set x/y labels and plot title
+plt.pyplot.xlabel("horsepower")
+plt.pyplot.ylabel("count")
+plt.pyplot.title("horsepower bins")
+
+plt.pyplot.show()
+
+## Categorical values to numerical values
+
+
+dummy_variable_1 = pd.get_dummies(df["fuel-type"])
+print(dummy_variable_1.head())
+
+dummy_variable_1.rename(columns={'gas':'fuel-type-gas', 'diesel':'fuel-type-diesel'}, inplace=True)
+print(dummy_variable_1.head())
+
+# merge data frame "df" and "dummy_variable_1"
+df = pd.concat([df, dummy_variable_1], axis=1)
+print(df.head())
+# drop original column "fuel-type" from "df"
+df.drop("fuel-type", axis = 1, inplace=True)
+print(df.head())
+
+dummy_variable_2 = pd.get_dummies(df['aspiration'])
+
+# change column names for clarity
+dummy_variable_2.rename(columns={'std':'aspiration-std', 'turbo': 'aspiration-turbo'}, inplace=True)
+
+# show first 5 instances of data frame "dummy_variable_1"
+dummy_variable_2.head()
+
+# merge the new dataframe to the original datafram
+df = pd.concat([df, dummy_variable_2], axis=1)
+
+# drop original column "aspiration" from "df"
+df.drop('aspiration', axis = 1, inplace=True)
+
+df.to_csv('clean_df.csv')
+
