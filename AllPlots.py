01772bb080069e21b6368908a70db83f16ffad0c
divyakhatiyan <divyakhatiyan@gmail.com>
2025-03-18 19:32:45 +0530

    Plots added

--- AllPlots.py
+++ AllPlots.py
@@ -7,6 +7,11 @@ import seaborn as sns
 
 # Standard Line Plot
 
-plt.plot(x,y)
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+plt.plot(x, y)
+
+plt.show()
 
 #Scatter plot
