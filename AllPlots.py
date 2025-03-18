b97af305674166ad832708f3030b0f408470acab
divyakhatiyan <divyakhatiyan@gmail.com>
2025-03-18 19:47:36 +0530

    Plots added

--- AllPlots.py
+++ AllPlots.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 import seaborn as sns
 
-# Standard Line Plot
+#Standard Line Plot
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
@@ -15,3 +15,11 @@ plt.plot(x, y)
 plt.show()
 
 #Scatter plot
+
+x = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10, 10, 10]
+y = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 3, 1, 2, 4, 1, 2, 3, 2, 6, 5, 4, 1, 9, 5, 2, 6, 4, 3]
+
+plt.scatter(x, y)
+
+plt.show()
+
