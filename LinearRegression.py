98918e8a9a676e046a9a43091755459d4f31885c
divyakhatiyan <divyakhatiyan@gmail.com>
2025-04-06 22:39:58 +0530

    Linear Regression

--- LinearRegression.py
+++ LinearRegression.py
@@ -107,3 +107,26 @@ plt.plot(X_train, regressor.coef_ * X_train + regressor.intercept_, '-r')
 plt.xlabel("Engine size")
 plt.ylabel("Emission")
 plt.show()
+"Model evaluation"
+'''compare the actual values and predicted values to calculate the accuracy of a regression model. Evaluation metrics play a key role in the development of a model, as they provide insight into areas that require improvement.
+
+There are different model evaluation metrics, let's use MSE here to calculate the accuracy of our model based on the test set:
+
+    Mean Absolute Error: It is the mean of the absolute value of the errors. This is the easiest of the metrics to understand since it’s just an average error.
+
+    Mean Squared Error (MSE): MSE is the mean of the squared error. In fact, it's the metric used by the model to find the best fit line, and for that reason, it is also called the residual sum of squares.
+
+    Root Mean Squared Error (RMSE). RMSE simply transforms the MSE into the same units as the variables being compared, which can make it easier to interpret.
+
+    R-squared is not an error but rather a popular metric used to estimate the performance of your regression model. It represents how close the data points are to the fitted regression line. The higher the R-squared value, the better the model fits your data. The best possible score is 1.0 and it can be negative (because the model can be arbitrarily worse).
+'''
+
+import sklearn.metrics
+
+# Use the predict method to make test predictions
+y_test_ = regressor.predict( X_test.reshape(-1,1))
+
+print("Mean absolute error: %.2f" % sklearn.metrics.mean_absolute_error(y_test_, y_test))
+print("Mean squared error: %.2f" % sklearn.metrics.mean_squared_error(y_test_, y_test))
+#print("Root mean squared error: %.2f" % root_mean_squared_error(y_test_, y_test))
+print("R2-score: %.2f" % sklearn.metrics.r2_score(y_test_, y_test))
---
+++ MultipleRegression.py
@@ -0,0 +1,10 @@
+'''
+Dependencies
+!pip install numpy==2.2.0
+!pip install pandas==2.2.3
+!pip install scikit-learn==1.6.0
+!pip install matplotlib==3.9.3
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
