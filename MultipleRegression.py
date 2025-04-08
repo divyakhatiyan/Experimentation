5aef8e98488f0bb19294f1a971faf89a3cec9421
divyakhatiyan <divyakhatiyan@gmail.com>
2025-04-08 21:37:33 +0530

    Multiple Regression

--- MultipleRegression.py
+++ MultipleRegression.py
@@ -116,4 +116,109 @@ intercept_ = regressor.intercept_
 print ('Coefficients: ',coef_)
 print ('Intercept: ',intercept_)
 
+# Get the standard scaler's mean and standard deviation parameters
+means_ = std_scaler.mean_
+std_devs_ = np.sqrt(std_scaler.var_)
 
+# The least squares parameters can be calculated relative to the original, unstandardized feature space as:
+coef_original = coef_ / std_devs_
+intercept_original = intercept_ - np.sum((means_ * coef_) / std_devs_)
+
+print ('Coefficients: ', coef_original)
+print ('Intercept: ', intercept_original)
+'''Visualize model outputs
+
+You can visualize the goodness-of-fit of the model to the training data by plotting the fitted plane over the data.
+'''
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Ensure X1, X2, and y_test have compatible shapes for 3D plotting
+X1 = X_test[:, 0] if X_test.ndim > 1 else X_test
+X2 = X_test[:, 1] if X_test.ndim > 1 else np.zeros_like(X1)
+
+# Create a mesh grid for plotting the regression plane
+x1_surf, x2_surf = np.meshgrid(np.linspace(X1.min(), X1.max(), 100),
+                               np.linspace(X2.min(), X2.max(), 100))
+
+y_surf = intercept_ +  coef_[0,0] * x1_surf  +  coef_[0,1] * x2_surf
+
+# Predict y values using trained regression model to compare with actual y_test for above/below plane colors
+y_pred = regressor.predict(X_test.reshape(-1, 1)) if X_test.ndim == 1 else regressor.predict(X_test)
+above_plane = y_test >= y_pred
+below_plane = y_test < y_pred
+above_plane = above_plane[:,0]
+below_plane = below_plane[:,0]
+
+# Plotting
+fig = plt.figure(figsize=(20, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the data points above and below the plane in different colors
+ax.scatter(X1[above_plane], X2[above_plane], y_test[above_plane],  label="Above Plane",s=70,alpha=.7,ec='k')
+ax.scatter(X1[below_plane], X2[below_plane], y_test[below_plane],  label="Below Plane",s=50,alpha=.3,ec='k')
+
+# Plot the regression plane
+ax.plot_surface(x1_surf, x2_surf, y_surf, color='k', alpha=0.21,label='plane')
+
+# Set view and labels
+ax.view_init(elev=10)
+
+ax.legend(fontsize='x-large',loc='upper center')
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_zticks([])
+ax.set_box_aspect(None, zoom=0.75)
+ax.set_xlabel('ENGINESIZE', fontsize='xx-large')
+ax.set_ylabel('FUELCONSUMPTION', fontsize='xx-large')
+ax.set_zlabel('CO2 Emissions', fontsize='xx-large')
+ax.set_title('Multiple Linear Regression of CO2 Emissions', fontsize='xx-large')
+plt.tight_layout()
+plt.show()
+
+plt.scatter(X_train[:,0], y_train,  color='blue')
+plt.plot(X_train[:,0], coef_[0,0] * X_train[:,0] + intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+
+plt.scatter(X_train[:,1], y_train,  color='blue')
+plt.plot(X_train[:,1], coef_[0,1] * X_train[:,1] + intercept_[0], '-r')
+plt.xlabel("FUELCONSUMPTION_COMB_MPG")
+plt.ylabel("Emission")
+plt.show()
+
+
+X_train_1 = X_train[:,0]
+regressor_1 = linear_model.LinearRegression()
+regressor_1.fit(X_train_1.reshape(-1, 1), y_train)
+coef_1 =  regressor_1.coef_
+intercept_1 = regressor_1.intercept_
+print ('Coefficients: ',coef_1)
+print ('Intercept: ',intercept_1)
+
+plt.scatter(X_train_1, y_train,  color='blue')
+plt.plot(X_train_1, coef_1[0] * X_train_1 + intercept_1, '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+
+X_test_1 = X_test[:,0]
+plt.scatter(X_test_1, y_test,  color='blue')
+plt.plot(X_test_1, coef_1[0] * X_test_1 + intercept_1, '-r')
+plt.xlabel("Engine size")
+plt.ylabel("CO2 Emission")
+
+X_train_2 = X_train[:,1]
+regressor_2 = linear_model.LinearRegression()
+regressor_2.fit(X_train_2.reshape(-1, 1), y_train)
+coef_2 =  regressor_2.coef_
+intercept_2 = regressor_2.intercept_
+print ('Coefficients: ',coef_2)
+print ('Intercept: ',intercept_2)
+
+X_test_2 = X_test[:,1]
+plt.scatter(X_test_2, y_test,  color='blue')
+plt.plot(X_test_2, coef_2[0] * X_test_2 + intercept_2, '-r')
+plt.xlabel("combined Fuel Consumption (MPG)")
+plt.ylabel("CO2 Emission")
